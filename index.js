a7d0ce0134da287d843f2927b598b51e02ceef9a
reoharada <reoharada@yahoo.co.jp>
2023-02-12 22:58:46 +0900

    axiosからfetchに切り替え

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const axios = require('axios');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const accessToken = process.argv[2];
 const fs = require('fs');
 const fsExtra = require('fs-extra');
 const slackUrl = 'https://slack.com/api/emoji.list';
-const params = {
-    token: accessToken,
-    pretty: 1
-};
+const params = { pretty: 1 };
 
 (async () => {
-    try {
-        console.log('start');
+  try {
+    console.log('start');
 
-        await fsExtra.remove('imgs');
-        fs.mkdirSync('imgs');
+    await fsExtra.remove('imgs');
+    fs.mkdirSync('imgs');
 
-        const query = Object.keys(params).map( v => { return `${v}=${params[v]}`; }).join('&');
-        const result = await axios.get(`${slackUrl}?${query}`);
+    const query = (new URLSearchParams(params)).toString();
+    const result = await fetch(`${slackUrl}?${query}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+    const obj = await result.json();
+    if (!obj.emoji) throw 'request key emoji is not exist.';
+    const emoji = obj.emoji;
 
-        const keys = Object.keys(result.data.emoji);
-        for(i in keys) {
-            const key = keys[i];
-            const imageUrl = result.data.emoji[key];
-            console.log(`${key}:${imageUrl}`);
-            if (/http/.test(imageUrl)) {
-                const imageResult = await axios.get(imageUrl, {responseType: 'arraybuffer'});
-                fs.writeFileSync(`imgs/${key}.png`, new Buffer.from(imageResult.data), 'binary');
-            }
-        }
-
-        console.log('finish');
-    } catch(e) {
-        console.log(e);
+    const keys = Object.keys(emoji);
+    for (const key of keys) {
+      const imageUrl = emoji[key];
+      console.log(`${key}:${imageUrl}`);
+      if (/http/.test(imageUrl)) {
+        const imageResult = await fetch(imageUrl, { method: 'GET' });
+        const arrayBuffer = await imageResult.arrayBuffer();
+        fs.writeFileSync(`imgs/${key}.png`, new Buffer.from(arrayBuffer), 'binary');
+      }
     }
+    console.log('finish');
+  } catch (e) {
+    console.log(e);
+  }
 })();
